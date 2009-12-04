4827740dada111a11d64ee7c915d890b82d0706b
chuby.qc <chuby.qc@f974c392-c36f-11de-a57c-69773c17b326>
2009-12-04 03:46:33 +0000

    - Adds required code in order to alert server that the client just booted.

--- trunk/client/src/chubyqc/gaeDistributed/client/network/ComManager.java
+++ trunk/client/src/chubyqc/gaeDistributed/client/network/ComManager.java
@@ -3,19 +3,29 @@ package chubyqc.gaeDistributed.client.network;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 
-import chubyqc.gaeDistributed.client.messages.incoming.ClientMessageFactory;
+import chubyqc.gaeDistributed.client.network.messages.incoming.ClientMessageFactory;
+import chubyqc.gaeDistributed.client.network.messages.outgoing.ClientBooted;
+import chubyqc.gaeDistributed.server.network.messages.incoming.MessageFactory;
+import chubyqc.gaeDistributed.server.network.messages.outgoing.OutgoingMessage;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
-public class ComManager {
-	private static ComManager _instance = new ComManager();
+public class ComManager extends chubyqc.gaeDistributed.server.network.ComManager {
+	private static ComManager _instance = new ComManager(ClientMessageFactory.getInstance());
 	public static ComManager getInstance() {
 		return _instance;
 	}
 	
-	private ComManager() {
+	private static final String SERVER_ADDRESS = "http://localhost:8181/dafti/incoming";
+	
+	private ComManager(MessageFactory factory) {
+		super(factory);
+	}
+	
+	public void send(OutgoingMessage message) {
+		send(SERVER_ADDRESS, message);
 	}
 	
 	public void start() {
@@ -29,13 +39,12 @@ public class ComManager {
 							
 							@Override
 							public void handle(HttpExchange content) throws IOException {
-								System.out.println("Incoming request");
-								ClientMessageFactory.getInstance().create(
-										content.getRequestBody()).execute();
+								receive(content.getRequestBody());
 							}
 						});
 						server.start();
 						System.out.println("Client started");
+						send(new ClientBooted());
 					} catch (IOException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
---
+++ trunk/client/src/chubyqc/gaeDistributed/client/network/messages/outgoing/ClientBooted.java
@@ -0,0 +1,9 @@
+package chubyqc.gaeDistributed.client.network.messages.outgoing;
+
+import chubyqc.gaeDistributed.server.network.messages.outgoing.OutgoingMessage;
+
+public class ClientBooted extends OutgoingMessage {
+	
+	public ClientBooted() {
+	}
+}
